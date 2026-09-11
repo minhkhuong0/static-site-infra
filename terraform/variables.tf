1e8f96c7dfd4c07bf72cd3d8ddeb89c9414c44a8
Khuong Nguyen <minhkhuongop@gmail.com>
2026-09-11 13:53:32 +0200

    feat(terraform): add cloudflare provider

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,24 @@ variable "region" {
   description = "Region for the resource deployment"
   default     = "eu-central-1"
 }
+
+variable "cf_api_token" {
+  type        = string
+  description = "API Token for Cloudflare"
+  sensitive   = true
+}
+
+variable "cf_zone_id" {
+  type        = string
+  description = "Cloudflare zone ID"
+}
+
+variable "cf_account_id" {
+  type        = string
+  description = "Cloudflare account ID"
+}
+
+variable "domain" {
+  type        = string
+  description = "Domain name"
+}
