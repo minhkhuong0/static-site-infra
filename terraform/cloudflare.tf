6b7cdb24b13c1ad84f8e9effca28938173dd143a
Khuong Nguyen <minhkhuongop@gmail.com>
2026-09-11 18:45:53 +0200

    chore: remove test /srv/portfolio/content/index.html

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -20,40 +20,23 @@ resource "cloudflare_dns_record" "root" {
   proxied = true
 }
 
-# resource "cloudflare_ruleset" "root_to_www" {
-#   zone_id     = var.cf_zone_id
-#   name        = "Redirect root to www ruleset"
-#   description = "Redirect root domain requests to www ruleset"
-#   kind        = "zone"
-#   phase       = "http_request_dynamic_redirect"
-#
-#   rules = [
-#     {
-#       ref         = "redirect_root_to_www"
-#       description = "Redirects root domain requests to www"
-#       expression  = "(http.host eq \"${var.domain}\")"
-#       action      = "redirect"
-#       action_parameters = {
-#         from_value = {
-#           status_code = 301
-#           target_url = {
-#             expression = "concat(\"https://www.${var.domain}\", http.request.uri.path)"
-#           }
-#           preserve_query_string = true
-#         }
-#       }
-#     }
-#   ]
-# }
-
 resource "cloudflare_page_rule" "root_to_www" {
   zone_id = var.cf_zone_id
-  target  = "https://nhmk.de/*"
+  target  = "https://${var.domain}/*"
   status  = "active"
   actions = {
     forwarding_url = {
       url         = "https://www.nhmk.de/$${1}"
       status_code = 301
     }
+  }
+}
+
+resource "cloudflare_page_rule" "flexible_ssl" {
+  zone_id = var.cf_zone_id
+  target  = "https://www.${var.domain}/*"
+  status  = "active"
+  actions = {
+    ssl = "flexible"
   }
 }
